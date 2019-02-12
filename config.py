39fda5b9514f1a848b9f266e7603c6f42dc3da10
kalister5 <zainkalister@gmail.com>
2019-02-12 15:30:17 +0300

    add more information

--- config.py
+++ config.py
@@ -21,10 +21,10 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")       
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     # enable CSRF secret key
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nayioma:qwerty@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hazard:1234@localhost/pitch'
 
     @staticmethod
     def init_app(app):
@@ -32,14 +32,14 @@ class Config:
 
 
 class ProdConfig(Config):
-    
+
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nayioma:qwerty@localhost/pitch_test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hazard:1234@localhost/pitch_test'
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nayioma:qwerty@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hazard:1234@localhost/pitch'
     DEBUG = True
 
 config_options = {
@@ -47,5 +47,5 @@ config_options = {
 'production':ProdConfig,
 'test':TestConfig
 
-}       
+}
 # https://git.heroku.com/pitch-idea-83.git
