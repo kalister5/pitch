c90c72ca5041dbb7e5422685b6f720ef6f7a76ef
kalister5 <zainkalister@gmail.com>
2019-02-12 13:10:34 +0300

    add a test folder

---
+++ tests/test_comment.py
@@ -0,0 +1,27 @@
+import unittest
+from app.models import Comment,User
+from app import db
+
+class CommentModelTest(unittest.TestCase):
+    def setUp(self):
+        self.user_naiyoma = User(username = 'naiyoma',password = 'potato')
+        self.new_comment = Comment(description='comments',user = self.user_naiyoma)
+
+    # def tearDown(self):
+    #         Review.query.delete()
+    #         User.query.delete()
+
+    def test_check_instance_variables(self):
+        self.assertTrue(self.new_comment, Comment)
+        
+        # self.assertEquals(self.new_comment.user,self.user_naiyoma)
+
+    def test_save_comment(self):
+        self.new_comment.save_comment()
+        self.assertTrue(len(Comment.query.all())>0)
+
+    def test_get_comment_by_id(self):
+
+        self.new_comment.save_comment()
+        got_comments = Comment.get_comments(1234)
+        self.assertTrue(len(got_comments) > 0)        
---
+++ tests/test_pitch.py
@@ -0,0 +1,28 @@
+import unittest
+from app.models import Pitch,User
+from app import db
+
+class PitchModelTest(unittest.TestCase):
+    def setUp(self):
+        self.user_naiyoma = User(username = 'naiyoma',password = 'potato')
+        self.new_pitch = Pitch(name='cat',title='movie',description='moviereview',user =self.user_naiyoma, category='technology')
+
+    # def tearDown(self):
+    #     Pitch.query.delete()
+    #     User.query.delete()
+
+    def test_check_instance_variable(self):
+        self.assertEquals(self.new_pitch.name,'cat')
+        self.assertEquals(self.new_pitch.title,'movie')
+        self.assertEquals(self.new_pitch.description,'moviereview')
+        self.assertEquals(self.new_pitch.category, 'technology')
+        # self.assertEquals(self.new_pitch.user,self.user_naiyoma)  
+
+    def test_save_pitch(self):
+        self.new_pitch.save_pitch()
+        self.assertTrue(len(Pitch.query.all()) >0)
+
+    def test_get_pitch_by_id(self):
+        self.new_pitch.save_pitch()
+        got_pitch = Pitch.get_pitches(12345)
+        self.assertTrue(len(got_pitch) > 0)    
