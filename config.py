9546724567d1764f128763c25fa5a517fc8a1e25
kalister5 <zainkalister@gmail.com>
2019-02-12 13:12:50 +0300

    add more information

---
+++ config.py
@@ -0,0 +1,51 @@
+
+import os
+
+class Config:
+    """
+    General configuration parent class
+
+    contains configuration used in both production and development stages
+    """
+
+    ## os.environ.get() used to get movie_api_key and secret key
+    # MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    # MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")       
+    # enable CSRF secret key
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nayioma:qwerty@localhost/pitch'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class ProdConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nayioma:qwerty@localhost/pitch_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nayioma:qwerty@localhost/pitch'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+
+}       
+# https://git.heroku.com/pitch-idea-83.git
---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY=1234
+
+export MAIL_USERNAME='lankas.aurelia@gmail.com'
+export MAIL_PASSWORD='mansamusa'
+
+python3.6 manage.py server
